6b2b7b47c90b166b53e04607d8d16baa96453fa1
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-14 17:21:46 +1100

    Use useEffect to retrieve blogs and render to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/posts', {mode: 'cors'})
+    .then((response) => {
+      if (response.status >= 400) {
+        const error = new Error("Server Error");
+        error.status = response.status;
+        throw error;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setPosts(data.posts)
+    })
+    .catch(error => console.log(error.message))
+  }, []);
 
   return (
     <>
@@ -9,7 +25,14 @@ function App() {
         <h1>Blog Site</h1>
       </header>
       <main>
-
+        <ul>
+          {posts.map((post) => {
+            return <li key={post.id}>
+                      <h2>{post.title}</h2>
+                      <p>{post.content}</p>
+                   </li>
+          })}
+        </ul>
       </main>
       <footer>
         Made by Wade
