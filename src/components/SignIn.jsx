dd8c2e3c5638da753c7a1bd59b302495fb9fa02f
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-29 21:04:29 +1100

    Adjust input widths

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -26,15 +26,19 @@ const StyledSection = styled.section`
     width: 100%;
   }
 
-  & .passwordInputContainer, .usernameInputContainer {
+  & .passwordInputContainer{
     display: flex;
     align-items: center;
     position: relative;
-    width: 188px;
+    width: 200px;
+  }
+
+  & input {
+    width: 200px;
   }
 
   & input[id="password"] {
-    width: 100%;
+    width: 100%
   }
 
   & button[title="Toggle Password Visibility"] {
@@ -162,17 +166,15 @@ const SignIn = ({
           <ul>
             <li>
               <label htmlFor="username">Username </label>
-              <div className="usernameInputContainer">
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  required
-                  maxLength={30}
-                />
-              </div>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                maxLength={30}
+              />
             </li>
             <li>
               <label htmlFor="password">Password </label>
